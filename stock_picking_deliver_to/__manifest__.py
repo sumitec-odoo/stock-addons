714ec58fa05c8b52e36ee5350d40a329fa37488b
Juan Pablo Garza <juanp@juanpgarza.com>
2022-08-31 15:39:18 -0300

    [15.0][ADD] stock_picking_deliver_to

---
+++ stock_picking_deliver_to/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "stock_picking_deliver_to",
+    "summary": "",
+    "version": "15.0.1.0.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/juanpgarza/stock-addons",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": 
+        [
+            "stock",
+        ],
+    "data": [
+        'views/stock_picking_views.xml',        
+        ],
+    "installable": True,
+}
---
+++ stock_picking_deliver_to/models/stock_picking.py
@@ -0,0 +1,9 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    deliver_to = fields.Many2one("res.users",string="Entregar a: ")
